423d0090b7c2f41367defd3022bf8a92d9fae0ce
Travis Mick <lq@le1.ca>
2015-05-28 14:06:57 -0600

    arduino ide also hates inline...

--- common/ringbuffer.h
+++ common/ringbuffer.h
@@ -12,27 +12,27 @@ typedef struct ringbuffer_s {
 } ringbuffer;
 
 /* Initialize a ringbuffer on the given flat array. */
-inline ringbuffer rb_init(uint8_t *buffer, size_t len);
+void rb_init(ringbuffer* rb, uint8_t *buffer, size_t len);
 
 /* Pop n bytes from the ringbuffer into the flat array.
    Returns the number of bytes retrieved.
 */
-inline size_t rb_get(ringbuffer *rb, uint8_t *buffer, size_t n);
+size_t rb_get(ringbuffer *rb, uint8_t *buffer, size_t n);
 
 /* Peek n bytes of the ringbuffer into the flat array.
    Returns the number of bytes retrieved.
 */
-inline size_t rb_peek(const ringbuffer *rb, uint8_t *buffer, size_t n);
+size_t rb_peek(const ringbuffer *rb, uint8_t *buffer, size_t n);
 
 /* Push n bytes from the flat array into the ringbuffer.
    Returns 0 if there is insufficient space in the ringbuffer;
    otherwise returns the number of bytes written.
 */
-inline size_t rb_put(ringbuffer *rb, const uint8_t *buffer, size_t n);
+size_t rb_put(ringbuffer *rb, const uint8_t *buffer, size_t n);
 
 /* Remove n bytes from the ringbuffer.
    Returns 0 if there are less than n bytes available.
 */
-inline size_t rb_del(ringbuffer *rb, size_t n);
+size_t rb_del(ringbuffer *rb, size_t n);
 
 #endif
